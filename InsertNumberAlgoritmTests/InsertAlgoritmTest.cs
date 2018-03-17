041a22e65b8c218b8272ced42418c0bffe3ea593
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-03-17 19:33:12 +0300

    DZ_2_1

---
+++ InsertNumberAlgoritmTests/InsertAlgoritmTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsertNumberAlgoritm;
+using NUnit.Framework;
+
+namespace InsertNumberAlgoritmTests
+{
+    [TestFixture]
+    public class InsertAlgoritmTest
+    {
+        [TestCase(15,15,0,0, ExpectedResult = 15)]
+        [TestCase(8,15,0,0,ExpectedResult = 9)]
+        [TestCase(8,15,3,8,ExpectedResult = 120)]
+        [TestCase(10,15,1,3,ExpectedResult = 14)]
+        [TestCase(15,8,0,2,ExpectedResult = 8)]
+        public int Insert_Byte_From_I_To_J_Index(int numberSource,int numberIn,int i,int j)
+        {
+           return Algoritm.InsertNumber(numberSource, numberIn, i, j);
+            
+        }
+
+        [TestCase(10, 15, 1, 33)]
+        [TestCase(10, 15, -2, 6)]
+        [TestCase(10, 15, 4, 2)]
+        [TestCase(10, 15, -9, -15)]
+        public void IndexScopeTest(int numberSource,int numberIn,int i,int j)
+        {
+           Assert.That(()=> Algoritm.InsertNumber(numberSource, numberIn, i, j),Throws.TypeOf<ArgumentException>());
+            
+        }
+    }
+}
---
+++ NET.W.2018.Petrusevitch.02.1/InsertNumberAlgoritm/Algoritm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertNumberAlgoritm
+{
+    public class Algoritm
+    {
+        /// <summary>
+        /// Insert number algoritm whith bitarray
+        /// </summary>
+        /// <param name="numberSource">first number</param>
+        /// <param name="numberIn">second number</param>
+        /// <param name="i">start index</param>
+        /// <param name="j">end index</param>
+        /// <returns></returns>
+        public static int InsertNumber(int numberSource, int numberIn, int i, int j)
+        {
+            if (i<0 || i>31 || j<0 || j> 31 || i> j)
+            {
+                throw new ArgumentException();
+            }
+
+            var numberSourceBitArray = new BitArray(BitConverter.GetBytes(numberSource));
+            var numberInBitArray = new BitArray(BitConverter.GetBytes(numberIn));
+            var tempArray = new bool[32];
+            for (int k = 0; k < numberSourceBitArray.Length-1; k++)
+            {
+                if ((k<i)||(k>j))
+                {
+                    tempArray[k] = numberSourceBitArray[k];
+                }
+
+                if ((k>=i)&&(k<=j))
+                {
+                    tempArray[k] = numberInBitArray[k - i];
+                }
+                
+            }
+            var result = new BitArray(tempArray);
+            var byteResult = new byte[4];
+            result.CopyTo(byteResult,0);
+
+
+
+            return BitConverter.ToInt32(byteResult, 0);
+        }
+    }
+}
