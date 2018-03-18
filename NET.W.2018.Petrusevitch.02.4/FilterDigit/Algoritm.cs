80d2941126ff3ca74fe2cc00b86a4ecf3e554f89
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-03-18 22:32:40 +0300

    DZ_2_4

---
+++ NET.W.2018.Petrusevitch.02.4/FilterDigit/Algoritm.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace FilterDigit
+{
+    public class Algoritm
+    {
+        public static int[] Filter(int filterNumber, params int[] numberArray)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                if (Regex.IsMatch(numberArray[i].ToString(),$"[{filterNumber.ToString()}]"))
+                {
+                    var flag = true;
+                    foreach (var number in result)
+                    {
+                        if (number==numberArray[i])
+                        {
+                            flag = false;
+                        }
+                    }
+
+                    if (flag)
+                    {
+                        result.Add(numberArray[i]);
+                    }
+                }
+            }
+
+            
+            
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ NET.W.2018.Petrusevitch.02.4/FilterDigit/FilterDigitsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FilterDigit
+{
+    [TestFixture]
+    public class FilterDigitsTest
+    {
+        [TestCase(7, 1, 2, 3, 7, 4, 5, 6, 7, 68, 69, 70, 15, 17, ExpectedResult = new int[] { 7, 70, 17 })]
+        [TestCase(1, 20, 18, 56, 31, 98, 11, ExpectedResult = new int[] { 18, 31, 11 })]
+        [TestCase(0, 0, 15, 50, 05, 18, 91, ExpectedResult = new int[] { 0, 50 })]
+        [TestCase(2, 20, 15, 52, 50, 05, 62, 84, 18, 91, ExpectedResult = new int[] { 20,52,62 })]
+        [TestCase(3, 20, 15, 52, 50, 05,33, 62, 84, 18, 91, ExpectedResult = new int[] { 33 })]
+        [TestCase(50, 20, 15, 52, 50, 05, 62, 84, 18, 91, ExpectedResult = new int[] { 50 })]
+        public int[] AlgoritmTest(int filterNumber, params int[] numberArray)
+        {
+            return Algoritm.Filter(filterNumber, numberArray);
+        }
+    }
+}
