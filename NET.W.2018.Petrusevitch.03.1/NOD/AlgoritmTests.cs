177f38e35bc8ef41bff9b12d2e522da2af369416
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-03-19 23:11:06 +0300

    DZ_3_1 увклид без условий

---
+++ NET.W.2018.Petrusevitch.03.1/NOD/AlgoritmTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace NOD
+{
+    [TestFixture]
+    public class AlgoritmTests
+    {
+        [TestCase(45,30,ExpectedResult = 15)]
+        public int EuclidTest(params int[] number)
+        {
+            return Euclid.Algoritm(number);
+        }
+        
+    }
+}
---
+++ NET.W.2018.Petrusevitch.03.1/NOD/Euclid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOD
+{
+    public class Euclid
+    {
+        public static int Algoritm(params int[] numbers)
+        {
+            var result = 0;
+            foreach (var number in numbers)
+            {
+                result = GCD(result, number);
+            }
+
+            return result;
+        }
+
+        private static int GCD(int a, int b)
+        {
+            return b == 0 ? (a < 0 ? -a : a) : GCD(b, a % b);
+        }
+    }
+}
