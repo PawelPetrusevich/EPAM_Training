58ca7820ee3f11b46312bf1c45d7e508846b91ac
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-03-19 16:57:45 +0300

    DZ_2_5

---
+++ NET.W.2018.Petrusevitch.02.5/FindNthRoot/FindNthRootAlgoritm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNthRoot
+{
+    public class FindNthRootAlgoritm
+    {
+        public static double SqrtN(double number, int n, double precesion)
+        {
+            if (precesion > 1 || precesion < 1e-15)
+            {
+                throw new ArgumentException();
+            }
+
+            if (n<0)
+            {
+                throw new ArgumentException();
+            }
+
+            double x0;
+            double x1;
+            x1 = number;
+            x0 = x1;
+            x1 = x0 - (Math.Pow(x0, n) - number) / (Math.Pow(x0, n - 1) * n);
+            while (Math.Abs(x1 - x0) > precesion)
+            {
+                x0 = x1;
+                x1 = x0 - (Math.Pow(x0, n) - number) / (Math.Pow(x0, n - 1) * n);
+            }
+
+            return x1;
+        }
+    }
+}
---
+++ NET.W.2018.Petrusevitch.02.5/FindNthRoot/FindNthRootTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FindNthRoot
+{
+    [TestFixture]
+    public class FindNthRootTest
+    {
+        [TestCase(1, 5, 0.00001, ExpectedResult = 1)]
+        [TestCase(9, 2, 0.00001, ExpectedResult = 3)]
+        [TestCase(100,2,0.00001,ExpectedResult = 10)]
+        public double AlgoritmTest(double number, int n, double precesion)
+        {
+            return FindNthRootAlgoritm.SqrtN(number, n, precesion);
+        }
+
+        [TestCase(8, 15, -7)]
+        [TestCase(8,15,0.0000000000000000000000000000000000001)]
+        [TestCase(8,15,1.1)]
+        [Test]
+        public void AlgoritmExeptionTest(double number, int n, double precesion)
+        {
+            Assert.Throws<ArgumentException>(() => FindNthRootAlgoritm.SqrtN(number, n, precesion));
+        }
+    }
+}
