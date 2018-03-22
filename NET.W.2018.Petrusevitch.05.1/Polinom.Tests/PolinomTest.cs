4433eaa0a8e8e3d9ad70875a4583de77fcf025f0
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-03-22 23:04:48 +0300

    DZ_5_1 create polinom

---
+++ NET.W.2018.Petrusevitch.05.1/Polinom.Tests/PolinomTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Polinom.Tests
+{
+    [TestFixture]
+    public class PolinomTest
+    {
+        [TestCase(1, 2, 3, ExpectedResult = "1x^3+2x^2+3x^1")]
+        [TestCase(15, -12, 19, 48, ExpectedResult = "15x^4+(-12)x^3+19x^2+48x^1")]
+        public string Polinom_ToString_Test(params double[] coeffArray)
+        {
+            Polynomial polinom = new Polynomial(coeffArray);
+            return polinom.ToString();
+        }
+
+        //только полиномы одной степени!!!!
+        [TestCase(new double[] { 3, 2, 1 }, new double[] { 1, 2, 3, 10 }, ExpectedResult = "4x^3+4x^2+4x^1")]
+        public string Polinoms_AddOperatorOvveraid_Test(double[] firstArray, double[] secondArray)
+        {
+            Polynomial first = new Polynomial(firstArray);
+            Polynomial second = new Polynomial(secondArray);
+            return (first + second).ToString();
+        }
+    }
+}
---
+++ NET.W.2018.Petrusevitch.05.1/Polinom/Polynomial.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinom
+{
+    public sealed class Polynomial : ICloneable
+    {
+        private readonly double[] _coeff;
+
+        public Polynomial(double[] coeff)
+        {
+            this._coeff = coeff;
+        }
+
+        public double this[int n]
+        {
+            get => _coeff[n];
+            set => _coeff[n] = value;
+        }
+
+        public int Power => _coeff.Length;
+
+        public override string ToString()
+        {
+            if (_coeff.Length == 0)
+            {
+                return "0";
+            }
+            StringBuilder sb = new StringBuilder();
+
+
+            for (int i = 0; i < Power; i++)
+            {
+                sb.Append(_coeff[i] < 0 ? $"+({_coeff[i]})x^{Power - i}" : $"+{_coeff[i]}x^{Power - i}");
+            }
+
+            if (sb[0] == '+' || sb[0] == '-')
+            {
+                sb.Remove(0, 1);
+            }
+
+            return sb.ToString();
+        }
+
+        public static Polynomial operator +(Polynomial firstPolynomial, Polynomial secondPolynomial)
+        {
+            double[] coeff = new double[firstPolynomial.Power>secondPolynomial.Power? firstPolynomial.Power : secondPolynomial.Power];
+            Polynomial result = new Polynomial(coeff);
+            for (int i = coeff.Length-1; i >0; i--)
+            {
+                double a = 0;
+                double b = 0;
+                if (i<firstPolynomial.Power)
+                {
+                    a = firstPolynomial[i];
+                }
+
+                if (i<secondPolynomial.Power)
+                {
+                    b = secondPolynomial[i];
+                }
+                result[coeff.Length-i] = a+b;
+            }
+
+            return result;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
