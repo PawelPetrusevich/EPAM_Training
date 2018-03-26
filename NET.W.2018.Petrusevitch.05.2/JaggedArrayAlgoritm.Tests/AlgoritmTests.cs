e2679640b796000f2c267ba774090b6f71808edd
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-03-26 22:36:48 +0300

    DZ_5_2

---
+++ NET.W.2018.Petrusevitch.05.2/JaggedArrayAlgoritm.Tests/AlgoritmTests.cs
@@ -0,0 +1,191 @@
+﻿namespace JaggedArrayAlgoritm.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AlgoritmTests
+    {
+        [Test]
+        public void SortUpSummElementTest()
+        {
+            int[][] array = new int[3][];
+            array[0] = new[] { 4, 3, 2, 1 };
+            array[1] = new[] { 6, 5, 4, 3, 2, 1 };
+            array[2] = new[] { 3, 2, 1 };
+
+            int[][] result = new int[3][];
+            result[0] = new[] { 1, 2, 3 };
+            result[1] = new[] { 1, 2, 3, 4 };
+            result[2] = new[] { 1, 2, 3, 4, 5, 6 };
+
+            int[][] array2 = new int[3][];
+            array2[0] = new[] { 9, 8, 3, 2 };
+            array2[1] = new[] { 10, 1, 5 };
+            array2[2] = new[] { 3, 2, 1 };
+
+            int[][] result2 = new int[3][];
+            result2[0] = new[] { 1, 2, 3 };
+            result2[1] = new[] { 1, 5, 10 };
+            result2[2] = new[] { 2, 3, 8, 9 };
+
+            JaggedArray.UpSumAlgoritm(array);
+            JaggedArray.UpSumAlgoritm(array2);
+
+            Assert.AreEqual(result, array);
+            Assert.AreEqual(result2, array2);
+
+        }
+
+
+
+        [Test]
+        public void SortDownSummElement()
+        {
+            int[][] array = new int[3][];
+            array[0] = new[] { 4, 3, 2, 1 };
+            array[1] = new[] { 6, 5, 4, 3, 2, 1 };
+            array[2] = new[] { 3, 2, 1 };
+
+            int[][] result = new int[3][];
+            result[0] = new[] { 1, 2, 3, 4, 5, 6 };
+            result[1] = new[] { 1, 2, 3, 4 };
+            result[2] = new[] { 1, 2, 3 };
+
+            int[][] array2 = new int[3][];
+            array2[0] = new[] { 9, 8, 3, 2 };
+            array2[1] = new[] { 10, 1, 5 };
+            array2[2] = new[] { 3, 2, 1 };
+
+            int[][] result2 = new int[3][];
+            result2[0] = new[] { 2, 3, 8, 9 };
+            result2[1] = new[] { 1, 5, 10 };
+            result2[2] = new[] { 1, 2, 3 };
+
+            JaggedArray.DownSumAlgoritm(array);
+            JaggedArray.DownSumAlgoritm(array2);
+
+            Assert.AreEqual(result, array);
+            Assert.AreEqual(result2, array2);
+        }
+
+        [Test]
+        public void SortUpMaxElement()
+        {
+            int[][] array = new int[3][];
+            array[0] = new[] { 4, 3, 2, 1 };
+            array[1] = new[] { 6, 5, 4, 3, 2, 1 };
+            array[2] = new[] { 3, 2, 1 };
+
+            int[][] result = new int[3][];
+            result[0] = new[] { 1, 2, 3 };
+            result[1] = new[] { 1, 2, 3, 4 };
+            result[2] = new[] { 1, 2, 3, 4, 5, 6 };
+
+            int[][] array2 = new int[3][];
+            array2[0] = new[] { 9, 8, 3, 2 };
+            array2[1] = new[] { 10, 1, 5 };
+            array2[2] = new[] { 3, 2, 1 };
+
+            int[][] result2 = new int[3][];
+            result2[0] = new[] { 1, 2, 3 };
+            result2[1] = new[] { 2, 3, 8, 9 };
+            result2[2] = new[] { 1, 5, 10 };
+
+            JaggedArray.UpMaxElemAlgoritm(array);
+            JaggedArray.UpMaxElemAlgoritm(array2);
+
+            Assert.AreEqual(result, array);
+            Assert.AreEqual(result2, array2);
+        }
+
+        [Test]
+        public void SortDownMaxElement()
+        {
+            int[][] array = new int[3][];
+            array[0] = new[] { 4, 3, 2, 1 };
+            array[1] = new[] { 6, 5, 4, 3, 2, 1 };
+            array[2] = new[] { 3, 2, 1 };
+
+            int[][] result = new int[3][];
+            result[0] = new[] { 1, 2, 3, 4, 5, 6 };
+            result[1] = new[] { 1, 2, 3, 4 };
+            result[2] = new[] { 1, 2, 3 };
+
+            int[][] array2 = new int[3][];
+            array2[0] = new[] { 9, 8, 3, 2 };
+            array2[1] = new[] { 10, 1, 5 };
+            array2[2] = new[] { 3, 2, 1 };
+
+            int[][] result2 = new int[3][];
+            result2[0] = new[] { 1, 5, 10 };
+            result2[1] = new[] { 2, 3, 8, 9 };
+            result2[2] = new[] { 1, 2, 3 };
+
+            JaggedArray.DownMaxElemAlgoritm(array);
+            JaggedArray.DownMaxElemAlgoritm(array2);
+
+            Assert.AreEqual(result, array);
+            Assert.AreEqual(result2, array2);
+        }
+
+        [Test]
+        public void SortUpMinElement()
+        {
+            int[][] array = new int[3][];
+            array[0] = new[] { 4, 3, 2, 1 };
+            array[1] = new[] { 6, 5, 4, 3, 2, 1 };
+            array[2] = new[] { 3, 2, 1 };
+
+            int[][] result = new int[3][];
+            result[0] = new[] { 1, 2, 3, 4 };
+            result[1] = new[] { 1, 2, 3, 4, 5, 6 };
+            result[2] = new[] { 1, 2, 3 };
+
+            int[][] array2 = new int[3][];
+            array2[0] = new[] { 9, 8, 3, 2 };
+            array2[1] = new[] { 10, 1, 5 };
+            array2[2] = new[] { 3, 2, 1 };
+
+            int[][] result2 = new int[3][];
+            result2[0] = new[] { 1, 5, 10 };
+            result2[1] = new[] { 1, 2, 3 };
+            result2[2] = new[] { 2, 3, 8, 9 };
+
+            JaggedArray.UpMinElemAlgoritm(array);
+            JaggedArray.UpMinElemAlgoritm(array2);
+
+            Assert.AreEqual(result, array);
+            Assert.AreEqual(result2, array2);
+        }
+
+        [Test]
+        public void SortDownMinElement()
+        {
+            int[][] array = new int[3][];
+            array[0] = new[] { 4, 3, 2, 1 };
+            array[1] = new[] { 6, 5, 4, 3, 2, 1 };
+            array[2] = new[] { 3, 2, 1 };
+
+            int[][] result = new int[3][];
+            result[0] = new[] { 1, 2, 3, 4 };
+            result[1] = new[] { 1, 2, 3, 4, 5, 6 };
+            result[2] = new[] { 1, 2, 3 };
+
+            int[][] array2 = new int[3][];
+            array2[0] = new[] { 9, 8, 3, 2 };
+            array2[1] = new[] { 10, 1, 5 };
+            array2[2] = new[] { 3, 2, 1 };
+
+            int[][] result2 = new int[3][];
+            result2[0] = new[] { 2, 3, 8, 9 };
+            result2[1] = new[] { 1, 5, 10 };
+            result2[2] = new[] { 1, 2, 3 };
+
+            JaggedArray.DownMinElemAlgoritm(array);
+            JaggedArray.DownMinElemAlgoritm(array2);
+
+            Assert.AreEqual(result, array);
+            Assert.AreEqual(result2, array2);
+        }
+    }
+}
---
+++ NET.W.2018.Petrusevitch.05.2/JaggedArrayAlgoritm/JaggedArray.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrayAlgoritm
+{
+    public class JaggedArray
+    {
+        /// <summary>
+        /// The down min elem sort algoritm.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        public static void DownMinElemAlgoritm(int[][] jaggedArray)
+        {
+
+            SortJaggedArray(jaggedArray);
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1 - i; j++)
+                {
+                    if (jaggedArray[j].Min() < jaggedArray[j + 1].Min())
+                    {
+                        var temp = jaggedArray[j];
+                        jaggedArray[j] = jaggedArray[j + 1];
+                        jaggedArray[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The up min elem sort algoritm.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        public static void UpMinElemAlgoritm(int[][] jaggedArray)
+        {
+
+            SortJaggedArray(jaggedArray);
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1 - i; j++)
+                {
+                    if (jaggedArray[j].Min() > jaggedArray[j + 1].Min())
+                    {
+                        var temp = jaggedArray[j];
+                        jaggedArray[j] = jaggedArray[j + 1];
+                        jaggedArray[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The down max elem sort algoritm.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        public static void DownMaxElemAlgoritm(int[][] jaggedArray)
+        {
+
+            SortJaggedArray(jaggedArray);
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1 - i; j++)
+                {
+                    if (jaggedArray[j].Max() < jaggedArray[j + 1].Max())
+                    {
+                        var temp = jaggedArray[j];
+                        jaggedArray[j] = jaggedArray[j + 1];
+                        jaggedArray[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The up max elem sort algoritm.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        public static void UpMaxElemAlgoritm(int[][] jaggedArray)
+        {
+
+            SortJaggedArray(jaggedArray);
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1 - i; j++)
+                {
+                    if (jaggedArray[j].Max() > jaggedArray[j + 1].Max())
+                    {
+                        var temp = jaggedArray[j];
+                        jaggedArray[j] = jaggedArray[j + 1];
+                        jaggedArray[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The down sum sort algoritm.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        public static void DownSumAlgoritm(int[][] jaggedArray)
+        {
+            SortJaggedArray(jaggedArray);
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1 - i; j++)
+                {
+                    if (jaggedArray[j].Sum() < jaggedArray[j + 1].Sum())
+                    {
+                        var temp = jaggedArray[j];
+                        jaggedArray[j] = jaggedArray[j + 1];
+                        jaggedArray[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The up sum sort algoritm.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        public static void UpSumAlgoritm(int[][] jaggedArray)
+        {
+            SortJaggedArray(jaggedArray);
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1 - i; j++)
+                {
+                    if (jaggedArray[j].Sum() > jaggedArray[j + 1].Sum())
+                    {
+                        var temp = jaggedArray[j];
+                        jaggedArray[j] = jaggedArray[j + 1];
+                        jaggedArray[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The bubble sort to array
+        /// </summary>
+        /// <param name="initArray">
+        /// The init array.
+        /// </param>
+        private static void BubbleSort(int[] initArray)
+        {
+            for (int i = 0; i < initArray.Length; i++)
+            {
+                for (int j = 0; j < initArray.Length - 1 - i; j++)
+                {
+                    if (initArray[j] > initArray[j + 1])
+                    {
+                        Swap(ref initArray[j], ref initArray[j + 1]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Swap the array element
+        /// </summary>
+        /// <param name="first">
+        /// The first element
+        /// </param>
+        /// <param name="second">
+        /// The second element
+        /// </param>
+        private static void Swap(ref int first, ref int second)
+        {
+            int temp = first;
+            first = second;
+            second = temp;
+        }
+
+        /// <summary>
+        /// The sort jagged array.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        private static void SortJaggedArray(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                BubbleSort(jaggedArray[i]);
+            }
+        }
+
+    }
+}
