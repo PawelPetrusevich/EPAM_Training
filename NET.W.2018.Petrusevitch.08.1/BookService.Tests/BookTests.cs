40b3026427558c0effc68e72c6a84a3fd1f9b2bf
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-04-07 16:27:50 +0300

    DZ_10_2and3

--- NET.W.2018.Petrusevitch.08.1/BookService.Tests/BookTests.cs
+++ NET.W.2018.Petrusevitch.08.1/BookService.Tests/BookTests.cs
@@ -41,5 +41,39 @@ namespace BookService.Tests
             Book book = new Book("Шилд", "C# 4.0", 1, 2010, 468, 280);
             Assert.Throws<ArgumentException>(() => book.ToString("PD"));
         }
+
+        [Test]
+        public void BookFormatterNPTest()
+        {
+            Book book = new Book("Шилд", "C# 4.0", 1, 2010, 468, (decimal)280.8954);
+            var result = string.Format(new BookFormat(), "{0:NP}", book);
+            var expected = "Book name: C# 4.0. Book price:280,895";
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void BookFormatterINTest()
+        {
+            Book book = new Book("Шилд", "C# 4.0", 123456, 2010, 468, (decimal)280.8954);
+            var result = string.Format(new BookFormat(), "{0:IN}", book);
+            var expected = "ISBN: 123456. Book name:C# 4.0";
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void BookFormatterNoBookTest()
+        {
+            Book book = new Book("Шилд", "C# 4.0", 123456, 2010, 468, (decimal)280.8954);
+            Assert.Throws<ArgumentException>(() => string.Format(new BookFormat(), "{0:IN}", new int()));
+        }
+
+        [Test]
+        public void BookFormatterInvalidFormatTest()
+        {
+            Book book = new Book("Шилд", "C# 4.0", 123456, 2010, 468, (decimal)280.8954);
+            var result = string.Format(new BookFormat(), "{0:NG}", book);
+            var expected = "NG is invalid format";
+            Assert.That(result, Is.EqualTo(expected));
+        }
     }
 }
---
+++ NET.W.2018.Petrusevitch.08.1/Repository/BookFormat.cs
@@ -0,0 +1,89 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company=" " file=" BookFormat.cs">
+// 
+// </copyright>
+// <summary>
+//  Creator name: 
+//  Solution: BookService
+//  Project: Repository
+//  Filename: BookFormat.cs
+//  Created day: 07.04.2018    14:52
+//  </summary> 
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace Repository
+{
+    using System;
+
+    /// <summary>
+    /// custom provider for book.
+    /// </summary>
+    public class BookFormat : IFormatProvider, ICustomFormatter
+    {
+        /// <summary>
+        /// IFormatProvider interface implementation.
+        /// </summary>
+        /// <param name="formatType">format type</param>
+        /// <returns>return format</returns>
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// ICustomFormatter interface implementation
+        /// </summary>
+        /// <param name="format">
+        /// The format.
+        /// </param>
+        /// <param name="arg">
+        /// object as Book
+        /// </param>
+        /// <param name="formatProvider">
+        /// The format provider.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/> result string.
+        /// </returns>
+        /// <exception cref="ArgumentException">argument cheked
+        /// </exception>
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (!this.Equals(formatProvider))
+            {
+                throw new ArgumentException(nameof(formatProvider));
+            }
+
+            if (string.IsNullOrEmpty(format))
+            {
+                format = "NP";
+            }
+
+            Book book = arg as Book;
+
+            if (book == null)
+            {
+                throw new ArgumentException(nameof(arg));
+            }
+
+            if (format == "NP")
+            {
+                return $"Book name: {book.BookName}. Book price:{book.Price:##.###}";
+            }
+
+            if (format == "IN")
+            {
+                return $"ISBN: {book.ISBN}. Book name:{book.BookName}";
+            }
+
+            return $"{format} is invalid format";
+        }
+    }
+}
